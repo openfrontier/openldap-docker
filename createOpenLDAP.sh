211b24351dd468686eedec14fad1619c3b6321d2
zsx <thinkernel@gmail.com>
2015-05-27 13:09:49 +0000

    Integrate with ci project

--- createOpenLDAP.sh
+++ createOpenLDAP.sh
@@ -4,14 +4,27 @@ LDAP_NAME=${LDAP_NAME:-openldap}
 SLAPD_PASSWORD=${SLAPD_PASSWORD:-$1}
 SLAPD_DOMAIN=${SLAPD_DOMAIN:-$2}
 LDAP_IMAGE_NAME=${LDAP_IMAGE_NAME:-dinkel/openldap}
+GERRIT_ADMIN_UID=${GERRIT_ADMIN_UID:-$3}
+GERRIT_ADMIN_PWD=${GERRIT_ADMIN_PWD:-$4}
+GERRIT_ADMIN_EMAIL=${GERRIT_ADMIN_EMAIL:-$5}
 
 BASE_LDIF=base.ldif
-SLAPD_DOMAIN_DC=`echo ${SLAPD_DOMAIN}|sed 's/\./,dc=/g'`
-DC="dc="
 
-sed -e "s/{SLAPD_DOMAIN_DC}/${DC}${SLAPD_DOMAIN_DC}/g" ~/openldap-docker/${BASE_LDIF}.template > ~/openldap-docker/${BASE_LDIF}
-sed -i "s/{SLAPD_DOMAIN}/${SLAPD_DOMAIN}/g" ~/openldap-docker/${BASE_LDIF}
+#Convert FQDN to LDAP base DN
+SLAPD_TMP_DN=".${SLAPD_DOMAIN}"
+while [ -n "${SLAPD_TMP_DN}" ]; do
+SLAPD_DN=",dc=${SLAPD_TMP_DN##*.}${SLAPD_DN}"
+SLAPD_TMP_DN="${SLAPD_TMP_DN%.*}"
+done
+SLAPD_DN="${SLAPD_DN#,}"
 
+#Create base.ldif
+sed -e "s/{SLAPD_DN}/${SLAPD_DN}/g" ~/openldap-docker/${BASE_LDIF}.template > ~/openldap-docker/${BASE_LDIF}
+sed -i "s/{ADMIN_UID}/${GERRIT_ADMIN_UID}/g" ~/openldap-docker/${BASE_LDIF}
+sed -i "s/{ADMIN_PWD}/${GERRIT_ADMIN_PWD}/g" ~/openldap-docker/${BASE_LDIF}
+sed -i "s/{ADMIN_EMAIL}/${GERRIT_ADMIN_EMAIL}/g" ~/openldap-docker/${BASE_LDIF}
+
+#Start openldap
 docker run \
 --name ${LDAP_NAME} \
 -p 389:389 \
@@ -20,11 +33,15 @@ docker run \
 -v ~/openldap-docker/${BASE_LDIF}:/${BASE_LDIF}:ro \
 -d ${LDAP_IMAGE_NAME}
 
-sleep 5
+while [ -z "$(docker logs ${LDAP_NAME} 2>&1 | tail -n 4 | grep 'slapd starting')" ]; do
+    echo "Waiting openldap ready."
+    sleep 1
+done
+
+#Import accounts
+docker exec openldap \
+ldapadd -f /${BASE_LDIF} -x -D "cn=admin,${SLAPD_DN}" -w ${SLAPD_PASSWORD}
 
 docker exec openldap \
-ldapadd -f /${BASE_LDIF} -x -D "cn=admin,${DC}${SLAPD_DOMAIN_DC}" -w ${SLAPD_PASSWORD}
-
-docker exec openldap \
-ldappasswd -x -D "cn=admin,${DC}${SLAPD_DOMAIN_DC}" -w ${SLAPD_PASSWORD} -s ${SLAPD_PASSWORD} \
-"uid=gerrit,ou=accounts,${DC}${SLAPD_DOMAIN_DC}"
+ldappasswd -x -D "cn=admin,${SLAPD_DN}" -w ${SLAPD_PASSWORD} -s ${ADMIN_PWD} \
+"uid=${ADMIN_UID},ou=accounts,${SLAPD_DN}"
